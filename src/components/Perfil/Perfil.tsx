4b1155cf8f513f96fafd304a6758167e17a607c3
Rolandi <sd.rolandi@gmail.com>
2021-02-27 17:29:25 -0300

    feat: chalenge

--- src/components/Perfil/Perfil.tsx
+++ src/components/Perfil/Perfil.tsx
@@ -1,14 +1,15 @@
-import styles from '../../style/components/profile.module.css';
+import styles from "../../style/components/profile.module.css";
 
 export default function Profile() {
   return (
     <div className={styles.profleConteiner}>
       <img src="https://github.com/yurirolandi.png" alt="Yuri Rolandi" />
       <div>
-        <strong>Yuri Rolandi</strong> 
+        <strong>Yuri Rolandi</strong>
         <p>
-            <img src="icons/level.svg" alt="Level"/>
-            Level 1</p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1
+        </p>
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { CompletedChallenges } from "../components/CompletedChallenges/CompletedChallenges";
 import { Countdown } from "../components/Countdown/Countdown";
 import ExperienceBar from "../components/ExperienceBar/ExperienceBar";
+import { ChallengeBox } from "../components/ChallengeBox/ChallengeBox";
 import Profile from "../components/Perfil/Perfil";
 import styles from "../style/pages/Home.module.css";
 import Head from 'next/head';
@@ -19,7 +20,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
